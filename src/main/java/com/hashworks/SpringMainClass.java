e7f4ceacc6fe1bf870ba894fb03e5ccb07e8ab81
Chaitra <chaitra.hoogar@hashworks.co>
2017-07-26 10:13:16 +0530

    first commit

---
+++ src/main/java/com/hashworks/SpringMainClass.java
@@ -0,0 +1,13 @@
+package com.hashworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMainClass.class,args);
+	}
+
+}
